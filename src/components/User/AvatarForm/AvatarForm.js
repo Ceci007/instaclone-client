eb768b30490e7d627fd76e4ee44815534ed5888a
Ceci007 <cecibot9@gmail.com>
2022-04-21 13:59:37 -0400

    Installing and configuring react-dropzone dependency

--- src/components/User/AvatarForm/AvatarForm.js
+++ src/components/User/AvatarForm/AvatarForm.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
 
 export default function AvatarForm(props) {
     const { modalTitle, setIsOpen } = props;
 
+    const onDrop = useCallback((file) => {
+        console.log(file);
+    }, []);
+
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: "image/jpeg, image/jpg, image/png",
+        noKeyboard: true,
+        multiple: false,
+        onDrop
+    });
+
     return (
         <div className='avatar-form'>
+            <input {...getInputProps()} />
             <h2 className='avatar-form-title'>{`${modalTitle}`}</h2>
             <div className='avatar-buttons'>
-                <div className='avatar-button button-1'>
+                <div className='avatar-button button-1' {...getRootProps()}>
                     <div className='icon-avatar'>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
